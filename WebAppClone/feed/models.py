3a78c68f23263a5e2b3d5374b2f8ff8be2dbf8c9
georgeballasdev <georgeballasdev@github.com>
2022-12-10 04:01:36 +0200

    Add Comment model, improve feed template

--- WebAppClone/feed/models.py
+++ WebAppClone/feed/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class PostModel(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField(max_length=500)
     img = models.ImageField(upload_to='images/', blank= True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     last_edited_at = models.DateTimeField(auto_now=True)
+    liked_by = models.ManyToManyField(User, related_name='liked_by')
+
+class CommentModel(models.Model):
+    of_post = models.ForeignKey(PostModel, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
