7eaad327d70e173654f69bdbfc18fdbdc8b7ba4d
georgeballasdev <georgeballas.dev@gmail.com>
2023-02-18 05:20:40 +0200

    test

--- WebAppClone/WebAppClone/settings.py
+++ WebAppClone/WebAppClone/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = '#&6sxq7h$oz_2une9mhko&r619x(3wnpgc%l$u)9g#ul4qeyv*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
